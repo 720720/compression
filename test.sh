cce1982e3f95f22127b7aa229c90dd6731d534c1
Richard Sartori <richard.sartori@gmail.com>
2020-07-24 14:59:53 +0200

    awk

--- test.sh
+++ test.sh
@@ -346,7 +346,7 @@ start() {
     cp "$input" "$output"
   fi
 
-  compressor="$(awk -F, -v input="$input" -v compressor="$compressor" -v options="$options" 'BEGIN{if(options)compressor=compressor" "options}$3==input{compressor=$4" "compressor}END{print compressor}' output.txt)"
+  compressor=$(awk -F, -v input="$input" -v compressor="$compressor" -v options="$options" 'BEGIN{if(options)compressor=compressor" "options}$3==input{compressor=$4" "compressor}END{print compressor}' output.txt)
 
   timer="$(mktemp)"
   trap "rm -f $timer" EXIT HUP INT QUIT TERM
